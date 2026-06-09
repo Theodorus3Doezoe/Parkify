7ed258445cc30cfa6cbbf4c0eb27d0060d2a0063
Burger,Max M. <m.burger@student.fontys.nl>
2026-06-09 11:33:56 +0000

    Node payment

---
+++ nodes/node_payment/src/PaymentController.cpp
@@ -0,0 +1,26 @@
+#include "node_payment/include/Headers/PaymentController.h"
+#include <Arduino.h>
+PaymentController::PaymentController(ILicensePlateScanner* scanner, MCP2515* canBus) 
+: scanner(scanner)
+, canBus(canBus)
+{}
+
+void PaymentController::Run()
+{
+    String license = scanner->Scan();
+    if (license == "")
+    {
+        return;
+    }
+
+    struct can_frame txMsg;
+
+    int length = license.length();
+    char arr[length + 1];
+    strcpy(arr, license.c_str());
+
+    txMsg.can_id = 9;
+    txMsg.can_dlc = strlen(arr);
+
+    canBus->sendMessage(&txMsg);
+}
