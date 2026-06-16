3e3007e994954ac60e69b424742027425f092cdc
MaxBurger99 <m.burger@student.fontys.nl>
2026-06-16 11:24:10 +0200

    Some bug fixes

--- nodes/node_payment/include/Headers/PaymentController.h
+++ nodes/node_payment/include/Headers/PaymentController.h
@@ -3,12 +3,14 @@
 #include "../lib/common/ILicensePlateScanner.h"
 #include "../lib/common/can_config.h"
 #include "../lib/common/can_comms_protocol.h"
+#include "../lib/common/database/database_client.h"
 
 class PaymentController{
 
     private:
     ILicensePlateScanner* scanner;
     MCP2515* canBus;
+    DatabaseClient dbClient;
 
     public:
     PaymentController(ILicensePlateScanner* scanner, MCP2515* canBus);
